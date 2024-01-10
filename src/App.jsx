110b9d5b93c378733b84b5b60141b32536ac6847
prasanth2817 <prasanth2817@gmail.com>
2024-01-10 23:43:21 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./Components/Login";
+import SignUp from "./Components/SignUp";
+import Dashboard from "./Dashboard";
+import ForgotPassword from "./Components/ForgotPassword";
+import ResetPassword from "./Components/ResetPassword";
+
+function App() {
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:token" element={<ResetPassword />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import AxiosService from "./Common/ApiService";
+import { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+
+function Dashboard() {
+  const [data, setData] = useState([]);
+  const getUsers = async () => {
+    try {
+      let res = await AxiosService.get(`/user`);
+      console.log(res);
+      if (res.status === 200) console.log(res.data.users);
+      setData(res.data.users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((e, i) => {
+            return (
+              <tr key={e._id}>
+                <td>{i + 1}</td>
+                <td>{e.firstName}</td>
+                <td>{e.lastName}</td>
+                <td>{e.email}</td>
+                <td>{e.role}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default Dashboard;
